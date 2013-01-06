5567f96e84a70392981a18c60022b0a67221f579
Tjerk Valentijn <tjercus@casema.nl>
2013-01-06 08:49:20 +0100

    added test for datum, needs impl

---
+++ client/test/datum.spec.js
@@ -0,0 +1,22 @@
+describe("EST.Datum", function() {
+  
+	describe("#secondsToTime", function() {
+
+		it("should ", function() {
+		
+		});
+	});
+
+	describe("#formatDate", function() {
+    it("should ", function() {
+		
+		});	
+	});
+
+	describe("#lpad", function() {
+
+		it("should ", function() {
+		
+		});
+	});
+});
