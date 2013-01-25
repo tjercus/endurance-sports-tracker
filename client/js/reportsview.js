282ee7c8b58404fc7907955dd181955152a58f7c
tjerk <tjerk@tjerk-Latitude-E5520.(none)>
2013-01-25 10:42:10 +0100

    added expr impl of highcharts lib

--- client/js/reportsview.js
+++ client/js/reportsview.js
@@ -12,13 +12,67 @@
 		initialize: function () {
 			var that = this;
 			this.collection.bind("reset", this.render, this);
-			this.collection.bind("add", this.render, this);			
+			this.collection.bind("add", this.render, this);
         },
 
 		render: function() {
+
+			// @todo replace expr chart function
+			distances = [];
+			_.each(EST.activities.models, function(act) {
+				distances.push(act.get('distance') / 1000);
+			});
+
+			$("<div id='chart'></div>").appendTo("#content");
+			
+        	new Highcharts.Chart({
+            chart: {
+                renderTo: 'chart',
+                type: 'column'
+            },
+            title: {
+                text: 'Distance per activity'
+            },
+            xAxis: {
+                categories: [
+                ]
+            },
+            yAxis: {
+                min: 1,
+                title: {
+                    text: 'Distance'
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                backgroundColor: '#FFFFFF',
+                align: 'left',
+                verticalAlign: 'top',
+                x: 100,
+                y: 70,
+                floating: true,
+                shadow: true
+            },
+            tooltip: {
+                formatter: function() {
+                    return this.y +' km';
+                }
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: 'volume',
+                data: distances
+            	}]
+        	});    
+
 			var json = this.collection.getData();
 			var html = _.template(this.template, json);
-			//console.log(html);
+			
 			this.$el.html(html);
 
 			return this;
